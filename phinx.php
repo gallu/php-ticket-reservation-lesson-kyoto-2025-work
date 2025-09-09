a5a1e0913a540df3c1a44d130528b2b1640f3a15
Michiaki Furusho <m.furusho@gmail.com>
2025-09-09 22:42:11 +0900

    初日初回用

---
+++ phinx.php
@@ -0,0 +1,23 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'furu_ticket',
+            'user' => 'furu_ticket_u',
+            'pass' => 'furu_ticket_p',
+            'port' => '3306',
+            'charset' => 'utf8mb4',
+        ],
+    ],
+    'version_order' => 'creation'
+];
